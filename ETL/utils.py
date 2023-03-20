f126464d880de0bab761cf87494aae5b8dc2a39d
JuanFe <jmartinezbernal02@gmail.com>
2023-03-19 21:52:36 -0500

    Organizar

--- ETL/utils.py
+++ ETL/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+# Limpiar el código de la partida arancelaria 
 def code_clean(x):
     code = str(x)
     
@@ -6,6 +9,7 @@ def code_clean(x):
     else:
         return code[:6]        
     
+# Limpiar el código de la sección
 def code_comm(x):
     code = str(x)
     
@@ -13,3 +17,17 @@ def code_comm(x):
         return code[:1]
     else:
         return code[:2]        
+    
+# Crear dataframes relacionales para columnas en concreto 
+def create_dimension(df, id_name):
+    list_keys = []
+    value = 1
+
+    for i in df:
+        list_keys.append(value)
+        value = value + 1
+
+    return pd.DataFrame({
+        id_name: list_keys,
+        'values': df
+    }) 
